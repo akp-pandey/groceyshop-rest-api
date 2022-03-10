d23b15c4c7c1a2d6097493cfd2af4fec13eccacd
abhabhishekp <meabhishekpandey308@gmail.com>
2022-03-10 10:51:08 +0530

    added cross origin

--- src/main/java/com/example/grocery/shop/controller/ProductController.java
+++ src/main/java/com/example/grocery/shop/controller/ProductController.java
@@ -19,6 +19,7 @@ import com.example.grocery.shop.webResponse.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,7 +44,7 @@ public class ProductController {
         pRepository.save(product);
         return new ProductResponse("Product added successfully", product);
     }
-
+    @CrossOrigin(origins ="http://localhost:4200")
     @GetMapping("/getAllProduct")
     public AllProductResponse getAllProduct(){
         List<Product> pList=pRepository.findAll();
